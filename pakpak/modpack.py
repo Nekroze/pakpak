6327debea009ca6c75c7f3a54ab079fcc278db8d
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-05-31 22:48:47 +1000

    Added and used check_files method to Modpack

--- pakpak/modpack.py
+++ pakpak/modpack.py
@@ -4,6 +4,7 @@ The Modpack class stores all information required to construct a modpack.
 __author__ = 'Taylor "Nekroze" Lawson'
 __email__ = 'nekroze@eturnilnetwork.com'
 from .packer import Packer
+import os
 
 
 class InplaceList(object):
@@ -21,10 +22,10 @@ class InplaceList(object):
         if isinstance(other, str):
             for line in other.split('\n'):
                 if line and not line.isspace():
-                    self.list.append(line.strip())
+                    self.list.append(os.path.join("mods", line.strip()))
         elif isinstance(other, (list, set, tuple)):
             for data in other:
-                self.list.append(data)
+                self.list.append(os.path.join("mods", data))
         else:
             self.list.append(other)
         return self
@@ -48,25 +49,48 @@ class InplaceList(object):
 
 class Modpack(object):
     """Modpack stores all types of data on how to construct the modpack."""
-    def __init__(self, basemodpack, baseserver, mods="mods/", output="build/"):
-        self.mods = mods
+    def __init__(self, basemodpack, baseserver, output="build/"):
+        self.mods = "mods/"
         self.output = output
         self.modpack = InplaceList()
         self.server = InplaceList()
         self.universal_mods = InplaceList()
         self.universal_coremods = InplaceList()
+        self.universal_data = InplaceList()
         self.client_mods = InplaceList()
         self.client_coremods = InplaceList()
+        self.client_data = InplaceList()
         self.server_mods = InplaceList()
         self.server_coremods = InplaceList()
-        self.client_data = InplaceList()
         self.server_data = InplaceList()
-        self.server += baseserver
-        self.modpack += basemodpack
+        self.server.list += baseserver
+        self.modpack.list += basemodpack
         self.launcher = "java -server -Xmx1024M -jar server.jar nogui"
+
+    def check_files(self):
+        """Check if all specified files exist."""
+        files = self.modpack.list
+        files.extend(self.server.list)
+        files.extend(self.universal_mods.list)
+        files.extend(self.universal_coremods.list)
+        files.extend(self.universal_data.list)
+        files.extend(self.client_mods.list)
+        files.extend(self.client_coremods.list)
+        files.extend(self.client_data.list)
+        files.extend(self.server_mods.list)
+        files.extend(self.server_coremods.list)
+        files.extend(self.server_data.list)
+        missing = False
+        for single in files:
+            if not os.path.exists(single):
+                missing = True
+                print("[ERROR]Missing file: {0}".format(single))
+        if missing:
+            exit(1)
 
     def construct(self, client=True, server=True):
         """Construct the modpack using a packer."""
+        self.check_files()
         pack = Packer(self.mods, self.output,
                       self.client_data.list, self.server_data.list)
         clientmods = self.client_mods.list
