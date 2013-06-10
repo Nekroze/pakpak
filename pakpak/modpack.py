bb661c8ff7c7a40030fe45177bd3286092e1dd50
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-06-11 00:49:50 +1000

    Added commented file feature using '#' prefix means to completely ignore path

--- pakpak/modpack.py
+++ pakpak/modpack.py
@@ -27,6 +27,8 @@ class InplaceList(object):
                 if line and not line.isspace():
                     if line[0] == '@':
                         self.list.append(os.path.join("@components", line[1:]))
+                    if line[0] == '#':
+                        pass
                     else:
                         self.list.append(os.path.join("components", line))
 
@@ -35,6 +37,8 @@ class InplaceList(object):
                 line = line.strip()
                 if line[0] == '@':
                     self.list.append(os.path.join("@components", line[1:]))
+                if line[0] == '#':
+                    pass
                 else:
                     self.list.append(os.path.join("components", line))
         else:
